81fbcbde2e11c097488cceb8bdbb8a8836bd6e0b
HoangHai <hoangminhhai1997@gmail.com>
2019-08-04 21:10:53 +0700

    init project

---
+++ Teko-Dev-Test/Teko-Dev-Test/Classes/ViewControllers/Search/SearchVC.swift
@@ -0,0 +1,30 @@
+//
+//  SearchVC.swift
+//  Teko-Dev-Test
+//
+//  Created by Hoàng Hải on 8/4/19.
+//  Copyright © 2019 HoangHai. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
